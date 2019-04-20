2b85e1a67fa62bfd2838fecf50905c4f390b8fb7
ArcStatic <eband42@gmail.com>
2019-04-20 13:01:36 +0100

    1hr datasets gathered, 50ms, 100ms, 150ms data gathered for 5min runs

---
+++ mininet/chained-tests.sh
@@ -0,0 +1,22 @@
+#50min (10x5min)
+sudo python 50ms_quic_rtp_setup.py
+
+sleep 30s
+#50min (10x5min)
+sudo python 100ms_quic_rtp_setup.py
+
+sleep 30s
+#50min (10x5min)
+sudo python 150ms_quic_rtp_setup.py
+
+sleep 30s
+#3hrs (3x1hr)
+sudo python 1hr_50ms_quic_rtp_setup.py
+
+sleep 30s
+#3hrs (3x1hr)
+sudo python 1hr_100ms_quic_rtp_setup.py
+
+sleep 30s
+
+
